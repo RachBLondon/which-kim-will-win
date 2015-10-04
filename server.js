0b87e9c4715eccaa512e6194f5e0ba7d70558faa
Rachel Black <rachel.black1984@googlemail.com>
2015-10-04 14:34:44 +0100

    fixing bug in handler - now loads cat on button click

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync(__dirname + '/index.html');
-// var cat = fs.readFileSync(__dirname) + 'cat.html'); not sure why I cant create a html file and link it like index
-
+var cat = fs.readFileSync(__dirname + '/cat.html');
 
 // set the port for the server
 var port = process.env.PORT || 8000;
@@ -15,7 +14,7 @@ function handler(request, response) {
     response.end(index);
   } else if (url.indexOf('/cat') > -1) {
       // check if the url contains /cat and if so send back a link to a cat image e.g. from a database or an API
-      response.end('<img src="http://charts.stocktwits.com/production/original_24310845.jpg?1404265667">');
+      response.end(cat);
     }
 }
 
