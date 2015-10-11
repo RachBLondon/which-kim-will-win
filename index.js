dcbedd2e935a6242fcebcfc7cc8b4881bcb272c8
Rachel Black <rachel.black1984@googlemail.com>
2015-10-11 16:07:01 +0100

    connected to instgram API when hard coded, setup config.env to start to work with hidden APIs

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
+var request = require('request');
+var env = require('env2')('./config.env');
+
+
+var request = new XMLHttpRequest();
+
+//
 document.getElementById("startBtn").addEventListener("click", showBlocks);
+//
+
+
 
 function showBlocks(){
-  var picOne = document.createElement("img");
-  var picTwo = document.createElement("img");
-  picOne.src = 'http://a.abcnews.com/images/Entertainment/ht_kim_kardashian_instagram_jc_150224_1x1_1600.jpg';
-  picTwo.src = 'http://cdn2.thegloss.com/wp-content/uploads/2015/03/Kim-Kardashians-Pouty-Lips-Instagram-Photo.jpg';
-  document.querySelector("#photoOne").appendChild(picOne);
-  document.querySelector("#photoTwo").appendChild(picTwo);
+  //remove children
+  console.log(process.env.apikey);
+  var script = document.createElement('script');
+  script.src = "https://api.instagram.com/v1/users/18428658/media/recent?access_token="+ env + "&callback=displayData";
+  document.body.appendChild(script);
+  // var picOne = document.createElement("img");
+  // var picTwo = document.createElement("img");
+  //replaced with real data from server.js
+  // document.querySelector("#photoOne").replaceChild(picOne);
+  // document.querySelector("#photoTwo").appendChild(picTwo);
+
 };
+
+
+function displayData(response){
+  console.log(response);
+  var oldInsta = document.getElementsByClassName("instaphotos");
+  var arrphotos = [];
+
+  for (index = oldInsta.length - 1; index >= 0; index--) {
+      oldInsta[index].parentNode.removeChild(oldInsta[index]);
+    }
+  // for loop is accessing nested photo urls and pushing into array
+  for(var i = 0; i<9;i++){
+    arrphotos.push(response['data'][i]['images']['low_resolution']['url']);
+  }
+  // for loop is creating an 'img' element for each array element,
+  // specifying a source and class for them, then appending them to the body
+  for(var j = 0 ; j < 9 ; j++){
+    var photoDiv = document.createElement("div");
+    var oImg = document.createElement("img");
+    photoDiv.setAttribute('id', 'photoDest' + [j]);
+    photoDiv.setAttribute('class', 'instaphotos');
+    document.getElementById("instagram").appendChild(photoDiv);
+    oImg.setAttribute('src', arrphotos[j]);
+    oImg.setAttribute('class', 'pUnit');
+    document.getElementById("photoDest" + [j]).appendChild(oImg);
+  }
+}
+
+
+//on click on photo function
