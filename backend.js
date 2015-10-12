1480c41fb45a24549de05a42fc7f88b19633181f
Rachel Black <rachel.black1984@googlemail.com>
2015-10-12 20:47:53 +0100

    updating backend.js

--- backend.js
+++ backend.js
@@ -2,6 +2,7 @@ var request = require('request');
 var env = require('env2')('./config.env');
 
 console.log( process.env.APIkey);
+
 var getKimInsta = function(word, callback) {
   request('https://api.instagram.com/v1/users/18428658/media/recent?access_token=' + process.env.APIkey, function(error, response, array) {
       if (error) {
@@ -10,7 +11,7 @@ var getKimInsta = function(word, callback) {
       } else if (!error && response.statusCode == 200) {
       var jsonObject = JSON.parse(array);
       console.log(jsonObject);
-      // callback(jsonObject[0].text);
+      callback(jsonObject[0].text);
     }
   });
 };
