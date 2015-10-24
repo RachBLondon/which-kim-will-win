fb753ee8f14bb153fea6dc3718bd0b05e9546be2
Rachel Black <rachel.black1984@googlemail.com>
2015-10-24 16:03:27 +0100

    selects random images from API

--- backend.js
+++ backend.js
@@ -1,17 +1,29 @@
 var request = require('request');
 var env = require('env2')('./config.env');
+var randomNum = [];
 
-console.log( process.env.APIkey);
-
+function getTwoRandom(){
+  if(randomNum.length >1){
+    randomNum = [];
+  }
+  console.log(randomNum);
+  var ranOne = Math.floor(Math.random() * 20) + 1;
+  var randtwo = Math.floor(Math.random() * 20) + 1;
+  while(ranOne === randtwo){
+  randtwo = Math.floor(Math.random() * 20) + 1;
+  }
+  randomNum.push(ranOne, randtwo);
+}
 
 var getKimInsta = function(callback) {
+  getTwoRandom();
   request('https://api.instagram.com/v1/users/18428658/media/recent?access_token=' + process.env.APIkey, function(error, response, array) {
       if (error) {
           alert('Sorry, there was an error!');
           // send an error to front end
       } else if (!error && response.statusCode == 200) {
       var jsonObject = JSON.parse(array);
-      var photo = jsonObject.data[0].images.standard_resolution.url + '>' + jsonObject.data[0].likes.count + '>' + jsonObject.data[5].images.standard_resolution.url + '>' + jsonObject.data[5].likes.count;
+      var photo = jsonObject.data[randomNum[0]].images.standard_resolution.url + '>' + jsonObject.data[randomNum[0]].likes.count + '>' + jsonObject.data[randomNum[1]].images.standard_resolution.url + '>' + jsonObject.data[randomNum[1]].likes.count;
       callback(photo);
 
     }
