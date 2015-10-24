7018638567e97ebf64c375c289d092bd33d58a25
Rachel Black <rachel.black1984@googlemail.com>
2015-10-24 16:39:04 +0100

    refactored code to take array of photo and likes outside of http request

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 (function(){
-  // var photoArr;
-  // var kimPhotoOne;
-  // var kimOneLikes;
-  // var kimPhotoTwo;
-  var kimPhotoOne = document.getElementById("photoOne")
-  var kimOneLikes;
-  var kimPhotoTwo = document.getElementById("photoTwo")
-  var kimTwoLikes;
-  document.getElementById("startBtn").addEventListener("click", showBlocks);
-  //
+  var kimPhotoOne = document.getElementById("photoOne");
+  var kimPhotoTwo = document.getElementById("photoTwo");
+  var photoOneArr = [];
+  var photoTwoArr = [];
 
+  document.getElementById("startBtn").addEventListener("click", showBlocks);
 
 
   function showBlocks() {
     var out = new XMLHttpRequest();
     out.onreadystatechange = function() {
       if (out.readyState === 4 && out.status === 200) {
+        if(photoOneArr.length != 0 || photoTwoArr.length !=0){
+          photoOneArr = [];
+          photoTwoArr = [];
+        }
         var photoArr = out.responseText.split(">");
-        kimPhotoOne.setAttribute('src', photoArr[0]);
-        kimOneLikes = photoArr[1];
-        kimPhotoTwo.setAttribute('src', photoArr[2]);
-        kimTwoLikes = photoArr[3];
+        console.log(photoArr);
+        photoOneArr.push(photoArr[0],photoArr[1]);
+        photoTwoArr.push(photoArr[2],photoArr[3]);
+        kimPhotoOne.setAttribute('src', photoOneArr[0]);
+        kimPhotoTwo.setAttribute('src', photoTwoArr[0]);
+        console.log(photoOneArr);
+        console.log(photoTwoArr);
       }
     };
     out.open('GET', '/play');
