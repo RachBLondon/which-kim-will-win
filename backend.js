d33a7784ddde572ba37e810e8d39a54f6b4d4347
Rachel Black <rachel.black1984@googlemail.com>
2015-11-06 18:06:48 +0000

    fixed problem with getting info from api, images sent via json objects

--- backend.js
+++ backend.js
@@ -1,32 +1,33 @@
 var request = require('request');
 var env = require('env2')('./config.env');
-var randomNum = [];
 
-function getTwoRandom(range){
-  if(randomNum.length >1){
-    randomNum = [];
-  }
+function getTwoRandom() {
   var ranOne = Math.floor(Math.random() * 19) + 1;
-  var randtwo = Math.floor(Math.random() * 19) + 1;
-  while(ranOne === randtwo){
-  randtwo = Math.floor(Math.random() * 19) + 1;
+  var randTwo = Math.floor(Math.random() * 19) + 1;
+  while(ranOne === randTwo){
+  randTwo = Math.floor(Math.random() * 19) + 1;
   }
-  randomNum.push(ranOne, randtwo);
-}
+  return { randomOne : ranOne,
+           randomTwo : randTwo
+         }
+};
 
 var getKimInsta = function(callback) {
-  getTwoRandom();
   request('https://api.instagram.com/v1/users/18428658/media/recent?access_token=' + process.env.APIkey, function(error, response, array) {
       if (error) {
-          alert('Sorry, there was an error!');
+          console.error();
           // send an error to front end
       } else if (!error && response.statusCode == 200) {
       var jsonObject = JSON.parse(array);
-      console.log("json length>>>>>",jsonObject.data.length);
-      console.log("random number>>>>>>",randomNum);
-      var photo = jsonObject.data[randomNum[0]].images.standard_resolution.url + '>' + jsonObject.data[randomNum[0]].likes.count + '>' + jsonObject.data[randomNum[1]].images.standard_resolution.url + '>' + jsonObject.data[randomNum[1]].likes.count;
-      callback(photo);
+      var numbers = getTwoRandom();
 
+      var photo = {
+        image1: jsonObject.data[numbers.randomOne].images.standard_resolution.url,
+        likes1: jsonObject.data[numbers.randomOne].likes.count,
+        image2: jsonObject.data[numbers.randomTwo].images.standard_resolution.url,
+        likes2: jsonObject.data[numbers.randomTwo].likes.count
+      }
+      callback(JSON.stringify(photo));
     }
   });
 };
